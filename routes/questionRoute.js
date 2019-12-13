91f1a59602f32aead8a4824876ec32adcc1fb42a
Ravik Ganguly <theravikganguly@gmail.com>
2019-12-13 17:00:16 +0530

    refactor: Update code style

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const router = express.Router();
 
+const router = express.Router();
 const Question = require("../models/Question");
 
 router.get("/", async (req, res) => {
-  try {
-    const questions = await Question.find({});
-    return res.status(200).send(questions);
-  } catch (err) {
-    return res.status(400).send("Error occurred while feteching data!");
-  }
+	try {
+		const questions = await Question.find({});
+		return res.status(200).send(questions);
+	} catch (err) {
+		return res.status(400).send("Error occurred while feteching data!");
+	}
 });
 
 router.post("/", async (req, res) => {
-  let question = new Question({
-    question: req.body.question,
-    options: req.body.options
-  });
-  question = await question.save();
-  res.status(200).send("Question successfully submitted");
+	let question = new Question({
+		question: req.body.question,
+		options: req.body.options
+	});
+	question = await question.save();
+	res.status(200).send(question);
 });
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,54 +1,54 @@
 const express = require("express");
-const router = express.Router();
 
+const router = express.Router();
 const User = require("../models/User");
 const Player = require("../models/Player");
 
 router.get("/", async (req, res) => {
-  if (req.session.id) {
-    const user = await User.findById(req.session.id);
-    if (user) return res.status(200).send(user.userId);
-    return res.status(400).send("User doesn't exist");
-  }
-  return res.status(400).send("User not registered");
+	if (req.session.id) {
+		const user = await User.findById(req.session.id);
+		if (user) return res.status(200).send(user.userId);
+		return res.status(400).send("User doesn't exist");
+	}
+	return res.status(400).send("User not registered");
 });
 
 router.post("/", async (req, res) => {
-  let user = new User({
-    name: req.body.name,
-    quiz: req.body.quiz
-  });
-  user = await user.save();
-  req.session.id = user.id;
-  return res.status(200).send(user.userId);
+	let user = new User({
+		name: req.body.name,
+		quiz: req.body.quiz
+	});
+	user = await user.save();
+	req.session.id = user.id;
+	return res.status(200).send(user.userId);
 });
 
 router.get("/:userId", async (req, res) => {
-  if (req.session.id) {
-    const user = await User.findById(req.session.id);
-    if (user)
-      return res.status(200).send({
-        isPlayer: false,
-        userData: user
-      });
-    return res.status(400).send("User not found");
-  }
-  const user = await User.findOne({ userId: req.params.userId });
-  if (user)
-    return res.status(200).send({
-      isPlayer: true,
-      userData: user
-    });
-  return res.status(400).send("User Quiz doesn't exist");
+	if (req.session.id) {
+		const user = await User.findById(req.session.id);
+		if (user)
+			return res.status(200).send({
+				isPlayer: false,
+				userData: user
+			});
+		return res.status(400).send("User not found");
+	}
+	const user = await User.findOne({ userId: req.params.userId });
+	if (user)
+		return res.status(200).send({
+			isPlayer: true,
+			userData: user
+		});
+	return res.status(400).send("User Quiz doesn't exist");
 });
 
 router.get("/:userId/results", async (req, res) => {
-  try {
-    let results = await Player.find({ userId: req.params.userId });
-    return res.status(200).send(results);
-  } catch (err) {
-    return res.status(400).send("Some error occurred");
-  }
+	try {
+		const results = await Player.find({ userId: req.params.userId });
+		return res.status(200).send(results);
+	} catch (err) {
+		return res.status(400).send("Some error occurred");
+	}
 });
 
 module.exports = router;
