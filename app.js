d4c14489749f9a37a20afcd2b67145bb6de724c7
Ravik Ganguly <theravikganguly@gmail.com>
2019-12-13 14:25:59 +0530

    chore: Add deployment configuration

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
+const config = require("config");
 
+const app = express();
 const userRoute = require("./routes/userRoute");
 const questionRoute = require("./routes/questionRoute");
 const playerRoute = require("./routes/playerRoute");
+
+if (
+	process.env.NODE_ENV === "production" &&
+	(!config.get("dbUsername") || !config.get("dbPassword"))
+) {
+	console.log("App Crashed! Environment Variables not provided");
+	process.exit(1);
+}
+
+const dbConnectionString =
+	process.env.NODE_ENV === "production"
+		? `mongodb+srv://${config.get("dbUsername")}:${config.get(
+				"dbPassword"
+		  )}@ravikcluster-aiykj.mongodb.net/test?retryWrites=true&w=majority`
+		: "mongodb://localhost/quiz_db";
+
+mongoose
+	.connect(dbConnectionString, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch(err => console.log("Error while connecting to MongoDB"));
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,26 +39,17 @@ app.use(bodyParser.json());
 
 // Cookie Session Middleware
 app.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    secret: "mySecretKey",
-    httpOnly: true
-  })
+	cookieSession({
+		maxAge: 24 * 60 * 60 * 1000,
+		secret: "mySecretKey",
+		httpOnly: true
+	})
 );
 
-mongoose
-  .connect("mongodb://localhost/quiz_db", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log("Error while connecting to MongoDB"));
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT}`);
+	console.log(`Server started on PORT ${PORT}`);
 });
 
 app.use("/api/user", userRoute);
